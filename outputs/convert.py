56ef2d9e8a0ed5b86d1c3c6021ac36149cf2c0e6
Mehul Raheja <rahejamehul@gmail.com>
2021-09-19 01:11:28 -0700

    Added audio files

--- outputs/convert.py
+++ outputs/convert.py
@@ -4,6 +4,9 @@ for file in os.listdir("./final"):
 	path = "./final/" + file
 	new_file = file.replace(".mp4",".gif")
 	new_path = f"./gifs/{new_file}"
-	if(os.path.exists(new_path)):
-		continue
-	os.system(f"ffmpeg -i {path} {new_path}")
+	new_file2 = file.replace(".mp4",".mp3")
+	new_path2 = f"./audios/{new_file2}"
+	if(not os.path.exists(new_path)):
+		os.system(f"ffmpeg -i {path} {new_path}")
+	if(not os.path.exists(new_path2)):
+		os.system(f"ffmpeg -i {path} {new_path2}")
