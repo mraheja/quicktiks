cd344cdfc41aef0db0ec64461611c7b2d17dc077
Mehul Raheja <rahejamehul@gmail.com>
2021-09-18 20:58:47 -0700

    Updated usage to command line

--- run.py
+++ run.py
@@ -2,24 +2,25 @@ import generate
 import crop
 import change_background
 import resound
+import sys
 
-model_id = "obama"
+img, img_ext, video, video_ext, background, background_ext = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6]
 
-img_path = "./inputs/images/obama.jpg"
-img_name = "obama"
+model_id = img
 
-video_path = "./inputs/videos/fortnite.mp4"
-video_name = "fortnite"
+img_path = f"./inputs/images/{img}.{img_ext}"
+img_name = img
 
-background_path = "inputs/backgrounds/rushmore.jpg"
+video_path = f"./inputs/videos/{video}.{video_ext}"
+video_name = video
 
-#generate.run_model(model_id,img_path, img_name, video_path, video_name)
+background_path = f"inputs/backgrounds/{background}.{background_ext}"
 
-#cropped_video_path = crop.crop_video(img_name, video_name)
-cropped_video_path = "./outputs/cropped/obama-fortnite.mp4"
+generate.run_model(model_id,img_path, img_name, video_path, video_name)
 
-#changed_background_path = change_background.change_background(img_name,video_name,cropped_video_path,background_path)
-changed_background_path = "./outputs/bg_changed/obama-fortnite.mp4"
+cropped_video_path = crop.crop_video(img_name, video_name)
+
+changed_background_path = change_background.change_background(img_name,video_name,cropped_video_path,background_path)
 
 final_path = resound.apply_original_sound(img_name, video_name, cropped_video_path, changed_background_path)
 
